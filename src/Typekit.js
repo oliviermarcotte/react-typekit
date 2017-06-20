bf2ac7c7ec500b1327e882c655c418ac9702a798
Olivier Marcotte <oliviermarcotte@scolab.com>
2017-06-20 12:45:36 -0400

    Force load when the Typekit loaded isn't ours

--- src/Typekit.js
+++ src/Typekit.js
@@ -1,16 +1,18 @@
 import intersection from 'lodash/intersection';
 import split from 'lodash/split';
+import get from 'lodash/get';
 import React, { Component } from 'react';
 import buildScript from './utilities/buildScript';
 
 class Typekit extends Component {
   componentDidMount() {
     // If typekit has not yet been executed, force it.
+    const { kitId } = this.props;
     const htmlClasses = split(document.querySelector('html').className, ' ');
     const typekitClasses = ['wf-loading', 'wf-active', 'wf-inactive'];
-    const isLoaded = intersection(htmlClasses, typekitClasses).length > 0;
+    const isLoaded = intersection(htmlClasses, typekitClasses).length > 0
+                     && get(window, 'Typekit.config.kt') === kitId;
     if (this.script && !isLoaded) {
-      const { kitId } = this.props;
       const script = document.createElement('script');
       script.innerHTML = buildScript(kitId);
       this.script.parentNode.replaceChild(script, this.script);
